81ce7ac8df15f308ada4fce9d04c184586e3b08d
latajacysmok <maksymles@gmail.com>
2023-02-08 15:43:22 +0100

    Console.WriteLine
    Let tradition be satisfied :)

---
+++ Hello_World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
