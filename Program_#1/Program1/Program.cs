5a9588d456e4ed5e1082f7618fc88da8a780e375
latajacysmok <maksymles@gmail.com>
2023-02-08 16:10:46 +0100

    Program_#1
    Having fun with a new language.

---
+++ Program_#1/Program1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string napis = "napis";
+            Console.Write("test "+ napis+"\n");
+
+            Console.Write("Podaj jak masz na imie: ");
+            string imie = Console.ReadLine();
+
+            Console.WriteLine("Cześć "+ imie);
+
+            Console.Write("Ile masz lat?: ");
+            int wiek = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ok czyli masz "+wiek+" lat");
+
+            int ileDo100 = 100 - wiek;
+
+            Console.WriteLine("czyli {1} do 100 pozostalo ci {0} lat", ileDo100, imie);
+
+            Console.WriteLine("podaj jakąś liczbe z przecinkiem:");
+            double liczba = double.Parse(Console.ReadLine());
+            Console.WriteLine("podana liczba do kwadratu to:"+liczba*liczba);
+
+            //podaj pierwsza liczbe: 2
+            //podaj drógą liczbe: 3
+
+            //2 + 3 = 5
+            //2 - 3 = -1
+            //2 * 3 = 6
+            //2 / 3 = ?? 0(int) lub 0,66666666(double)
+            //2 s 3 = 2 (reszta z dzielenie % - modulo)
+
+
+        }
+    }
+}
