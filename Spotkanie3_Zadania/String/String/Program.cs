5bdd36d26af898f38c097bffe879dd2bcf04d8d1
latajacysmok <maksymles@gmail.com>
2023-02-27 20:54:09 +0100

    użyj 2 innych metod do manipulacji na stringu Wincyj mi wyszło xD

--- Spotkanie3_Zadania/String/String/Program.cs
+++ Spotkanie3_Zadania/String/String/Program.cs
@@ -12,6 +12,7 @@ Console.Write("My dear user, I am asking you to enter a some text, it can be a w
 string word = Console.ReadLine();
 char[] wordArray = word.ToCharArray();
 Array.Reverse(wordArray);
+//Concatenate string using String.Join method 
 Console.Write($"Inverted yours text: {String.Join("", wordArray)}\n");
 Console.Write($"Your text in capital letters: {word.ToUpper()}\n");
 Console.Write($"Your text in lowercase: {word.ToLower()}\n");
@@ -23,7 +24,7 @@ for( int c = 0; c < word.Count(); c++)
 }
 
 //Concatenate String Using + Operator
-Console.WriteLine("Hello Dear user, " + "your string is " + word + ".");
+Console.WriteLine("Hello Dear user, " + "your string is: " + word + ".");
 //String Interpolation
 Console.WriteLine($"Hello Dear user, your string is: {word}.");
 //Concatenate string method
@@ -38,11 +39,23 @@ Console.WriteLine(sb.ToString());
 string usreName = "Stefek";
 string.Concat(usreName, word);
 
-//Concatenate string using String.Join method 
-int[] intArray = { 1, 3, 5, 7, 9 };
-String seperator = ", ";
-string result = "Int, ";
-result += String.Join(seperator, intArray);
-Console.WriteLine($"Result: {result}");
+Console.WriteLine("---------------------");
+// compare word and usreName
+Boolean result1 = word.Equals(usreName);
+Console.WriteLine($"Comparing whether \"{word}\" is equal to \"{usreName}\":\t {result1}");
+
+//returns the position of the specified character in the string
+Console.Write($"We are looking for the index of the first occurrence of the letter 'x': ");
+if (word.IndexOf("x") < 0) Console.WriteLine($"The specified letter \"x\" is missing in the string: {word}");
+else Console.WriteLine(word.IndexOf("x"));
+
+//returns substring of a string
+Console.WriteLine($"We print now substring which start from index 0 and end in 5 position: {word.Substring(0, 5)} ");
+
+//splits the string into substring
+Console.WriteLine($"We print now our string but splits a letter: ");
+foreach (string letter in word.Split("")) Console.Write(letter);
+
 Console.ReadKey();
+
 
