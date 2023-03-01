1a9e9d37c52a78c87677597fcc7898e011d33560
latajacysmok <maksymles@gmail.com>
2023-03-01 20:15:15 +0100

    change return value in ItNumber method.

--- Spotkanie3_Zadania/BMI/BMI/Program.cs
+++ Spotkanie3_Zadania/BMI/BMI/Program.cs
@@ -12,21 +12,15 @@ class Program
         string name = Console.ReadLine();
         while (true)
         {
-            int weight;
-            double height;
-
-            Console.Write($"Dear {name}, give me your weight: ");
-            weight = ItNumber();     //TODO: daj zabezpieczniea na np ujemna waga albo 100 metrów wzrostu.
-
-            Console.Write($"Dear {name}, tell me your height(in meters): ");
-            height = ItNumber();    //TODO: daj zabezpieczniea na np ujemna waga albo 100 metrów wzrostu.
-
+            double weight = WeightChecker(name);
+            double height = HeightChecker(name);
             double bmi = BmiCalculation(weight, height);
+
             Console.WriteLine($"Dear {name}, Your BMI is {Math.Round(bmi, 2)}. You have {EstimateWeight(bmi)}.");
             EndProgramme(name);
         }
     }
-    public static double BmiCalculation(int weight, double height)
+    public static double BmiCalculation(double weight, double height)
     {
         double bmi = weight / PowerCalculation(height);
         return bmi;                                               
@@ -62,19 +56,38 @@ class Program
             break;
         }
     }
-    public static int ItNumber()
+    public static double ItNumber()
     {
         while (true)
         {
-            if (int.TryParse(Console.ReadLine(), out int ifExit)) return ifExit;
-            else
-            {
-                Console.Write($"{ifExit.ToString()} is not a number, please try again: ");
-            }
+            if (double.TryParse(Console.ReadLine(), out double number)) return number;
+            else Console.Write($"{number.ToString()} is not a number, please try again: ");
         }
     }
 
-    public static void GoOutOrStay(int ifExit, string name)
+    public static double WeightChecker(string name)
+    {
+        while (true)
+        {
+            Console.Write($"Dear {name}, give me your weight: ");
+            double weight = ItNumber();
+            if (weight > 0 && weight < 251) return weight;
+            else Console.WriteLine("The given weight value is invalid. Lets try again.");
+        } 
+    }
+
+    public static double HeightChecker(string name)
+    {
+        while (true)
+        {
+            Console.Write($"Dear {name}, tell me your height(in meters): ");
+            double height = ItNumber();
+            if (height > 0 && height < 3) return height;
+            else Console.WriteLine("The given height value is invalid. Lets try again.");
+        }
+    }
+
+    public static void GoOutOrStay(double ifExit, string name)
     {
         while (true)
         {
