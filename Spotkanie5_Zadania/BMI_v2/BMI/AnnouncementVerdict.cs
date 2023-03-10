860a0304a72dd3da0ffedb1450f52b1bbdcd4bab
latajacysmok <maksymles@gmail.com>
2023-03-10 14:47:39 +0100

    Object-oriented approach

---
+++ Spotkanie5_Zadania/BMI_v2/BMI/AnnouncementVerdict.cs
@@ -0,0 +1,34 @@
+﻿namespace BMI
+{
+    class AnnouncementVerdict
+    {
+        public void ApplicationRunner()
+        {
+            ProgramData programData = new ProgramData();
+            BmiCalculator bmiCalculator = new BmiCalculator();
+            AppOption appOption = new AppOption();
+            string name = Console.ReadLine();
+            while (true)
+            {
+                double weight = programData.WeightChecker(name);
+                double height = programData.HeightChecker(name);
+                double bmi = bmiCalculator.BmiCalculation(weight, height);
+
+                Console.WriteLine($"Dear {name}, Your BMI is {Math.Round(bmi, 2)}. You have {EstimateWeight(bmi)}.");
+                appOption.ChoiceOptions(name);
+            }
+        }
+        public string EstimateWeight(double bmi)
+        {
+            switch (bmi)
+            {
+                case < 18.5:
+                    return "underweight";
+                case > 24.9:
+                    return "overweight";
+                default:
+                    return "weight in standard";
+            }
+        }
+    }
+}
---
+++ Spotkanie5_Zadania/BMI_v2/BMI/BmiCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace BMI
+{
+    class BmiCalculator
+    {
+        public double BmiCalculation(double weight, double height)
+        {
+            double bmi = weight / PowerCalculation(height);
+            return bmi;
+        }
+
+        public double PowerCalculation(double height)
+        {
+            double pow = Math.Pow(2.0, (double)height);
+            return pow;
+        }
+    }
+}
